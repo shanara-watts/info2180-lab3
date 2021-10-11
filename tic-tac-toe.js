3a448b75fb3d947dc7370c92a38460c9ab181168
Shanara Watts <watts_shan2637@live.com>
2021-10-10 20:10:06 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,24 +1,24 @@
-const player_X = 'X';
-const player_O = 'O';
+const player_X = "X";
+const player_O = "O";
+var xPlay = true;
 var array_X = [];
 var array_O = [];
-var xPlay = true;
 var currentPlays = [];
 
 const Wins = 
 [
-    [0,3,6],
     [0,1,2],
+    [0,3,6],
     [0,4,8],
-    [2,5,8],
     [0,3,6],
     [1,4,7],
+    [2,5,8],
     [3,4,5],
     [6,7,8],
     [2,4,6]
 ]
 
-document.addEventListener("DOMContentLoaded", function()
+document.addEventListener("DOMContentLoaded",function()
 {
     restartGame();
     var button = document.getElementsByClassName("btn");
@@ -27,55 +27,56 @@ document.addEventListener("DOMContentLoaded", function()
 })
 
 function restartGame()
-{
+{ 
     array_X = [];
     array_O = [];
     currentPlays = [];
 
     state = document.getElementById("status"); //Get element with ID status
-    state.innerHTML = "Hover your mouse over a square and click to play X or O.";
+    state.innerHTML = "Move your mouse over a square and click to play an X or an O.";
 
     var squares = document.getElementById("board").children; //Get all children of the board, which are the divs
     for (i = 0; i < squares.length; i++)
     {
-        squares[i].className = "squares"; //Style squares
+        squares[i].className = "square"; //Style squares
         squares[i].innerHTML = "";
-        squares[i].addEventListener('click', clickFunc, {once : true}); // Adds an onclick event listener to each square
-        squares[i].id = i; // Add number ID to each of the squares to check the wins
-        squares[i].addEventListener("mouseover", squareStyle);
-        squares[i].addEventListener("mouseout", removeHighlight);
+        squares[i].addEventListener('click',clickFunc,{once : true}); //Add an onclick event listener to each squares
+        squares[i].id = i; //Add number ID to each square to check wins
+        squares[i].addEventListener("mouseover",squareDesign);
+        squares[i].addEventListener("mouseout",noHighlight);
     }
 }
 
-function squareStyle(e)
+function squareDesign(e)
 {
-    //Adds hover class to the square, this changes the css
+    //Changes css by adding hover class to square
     e.target.classList.add("hover");
 }
 
-function removeHighlight(e)
+function noHighlight(e)
 {
-    //Removes hover class from the square, this chages the css
+    //Changes css by removing hover class to square
     e.target.classList.remove("hover");
 }
 
 function clickFunc(e)
 {
     var currentPlayer = xPlay ? player_X : player_O; //Once it's X's turn the current player = X
+    
     console.log(e);
     const square = e.target;
-    square.innerHTML = square.innerHTML + currentPlayer; //Put an X or O in the square
-    square.className = square.className + " " + currentPlayer; //Add the X or O to the class 
-
+    square.innerHTML = square.innerHTML + currentPlayer; //Put X or O in square
+    square.className = square.className + " " + currentPlayer; //Add X or O to  class
+    
     //Take note of the squares that have already been played
     if (xPlay)
     {
-        array_X.push(parseInt(square.id)); //X's moves
+        array_X.push(parseInt(square.id)) ; //X's Moves
         currentPlays = array_X;
     }
     else
     {
-        array_O.push(parseInt(square.id)); //X's moves
+        array_O.push(parseInt(square.id)) ; //O's Moves
         currentPlays = array_O;
     }
 
@@ -85,16 +86,20 @@ function clickFunc(e)
         state.classList.add("you-won");
         state.innerHTML = "Congratulations! " + currentPlayer + " is the Winner!";
     }
-
+    
     xPlay = xPlay ? false : true; //Changes to other player
+    
+    
 }
 
-function Winner(play)
-{
-    console.log(play);
-    return Win.some(winCombo => {//Checks if there is at least one winng combination that:
-        return winCombo.every(pos =>{
-            return play.includes(pos);
+function Winner(plays)
+{    
+    console.log(plays);
+    return Wins.some(winCombo => { //Check if there is a winning combination which has 
+        return winCombo.every(pos =>{  //all positions in the combination filled in the players' moves
+            return plays.includes(pos); //this checks each play against position being tried
+                
         })
-    })
+               
+    })   
 }
